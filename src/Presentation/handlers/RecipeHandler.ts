b87af186fbd46e1b646f5afe434e17a8185f205d
progriro <progriro@gmail.com>
2021-03-22 22:14:52 +0900

    [modified] for deploy

--- src/Presentation/handlers/RecipeHandler.ts
+++ src/Presentation/handlers/RecipeHandler.ts
@@ -1,8 +1,10 @@
 import { createMockRecipeRepository } from "@/Infrastructure";
+import { createRecipeRepository } from "@/Infrastructure";
 import { Recipe } from "@/Domain/Entity";
 
 export const RecipeHandler = () => {
-  const reciptClient = createMockRecipeRepository();
+  // const reciptClient = createMockRecipeRepository();
+  const reciptClient = createRecipeRepository();
 
   const fetchRecipes = async () => await reciptClient.fetchRecipes();
 
