946045bf8437562f2e79bbb9ffcfdd61617341f1
progriro <progriro@gmail.com>
2021-03-22 22:09:27 +0900

    [modified] 環境変数をenvから読み込む

--- src/Infrastructure/utils/RestClient.ts
+++ src/Infrastructure/utils/RestClient.ts
@@ -3,7 +3,7 @@ import { RestClientInterface } from "./RestClientInterface";
 export class RestClient implements RestClientInterface {
   async get<ResponseInterface>(path: string) {
     return fetch(path, {
-      headers: { "X-Api-Key": "remark-fish-magician" },
+      headers: { "X-Api-Key": process.env.API_KEY as string },
     }).then<ResponseInterface>(async (response) => await response.json());
   }
 }
