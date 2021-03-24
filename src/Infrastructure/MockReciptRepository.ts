5eeeae02e44f60c1d375d8e44c9745794f90a9e5
progriro <progriro@gmail.com>
2021-03-25 06:54:19 +0900

    [add] mockRepositoryにメソッド追加

--- src/Infrastructure/MockReciptRepository.ts
+++ src/Infrastructure/MockReciptRepository.ts
@@ -1,5 +1,5 @@
 import { recipesObj } from "@/__tests__/mocks";
-import { RecipeRepositoryInterface, Links } from "@/Domain/Repository";
+import { RecipeRepositoryInterface } from "@/Domain/Repository";
 import { RecipeFactory, RecipeObj } from "@/Domain/Factory";
 
 export class MockRecipeRepository implements RecipeRepositoryInterface {
@@ -16,6 +16,16 @@ export class MockRecipeRepository implements RecipeRepositoryInterface {
   public async fetchRecipe() {
     const recipe = RecipeFactory.createFromRecipeObj(recipesObj[0]);
     return recipe;
+  }
+
+  public async fetchRecipeWithRelated() {
+    const recipes = recipesObj.map((recipe: RecipeObj) =>
+      RecipeFactory.createFromRecipeObj(recipe)
+    );
+    return {
+      recipes: recipes,
+      links: {},
+    };
   }
 
   public async searchRecipes() {
